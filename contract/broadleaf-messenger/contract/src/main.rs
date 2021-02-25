043dbb28c0d395ecf83cd2a8a27202e5d3b31d6a
George <george@casperlabs.io>
2021-02-25 17:13:05 +0000

    Updates contract and adds deploy script

--- contract/broadleaf-messenger/contract/src/main.rs
+++ contract/broadleaf-messenger/contract/src/main.rs
@@ -9,10 +9,10 @@ use casper_contract::{
 };
 use casper_types::{Key, URef};
 
-const KEY: &str = "special_value";
-const ARG_MESSAGE: &str = "message";
+const SENDER: &str = "sender";
+const MESSAGE: &str = "message";
 
-fn store(value: String) {
+fn store_kv_pair(key: String, value: String) {
     // Store `value` under a new unforgeable reference.
     let value_ref: URef = storage::new_uref(value);
 
@@ -20,13 +20,14 @@ fn store(value: String) {
     let value_key: Key = value_ref.into();
 
     // Store this key under the name "special_value" in context-local storage.
-    runtime::put_key(KEY, value_key);
+    runtime::put_key(&key, value_key);
 }
 
 // All session code must have a `call` entrypoint.
 #[no_mangle]
 pub extern "C" fn call() {
     // Get the optional first argument supplied to the argument.
-    let value: String = runtime::get_named_arg(ARG_MESSAGE);
-    store(value);
+    let value: String = runtime::get_named_arg(MESSAGE);
+    let key: String = runtime::get_named_arg(SENDER);
+    store_kv_pair(key, value);
 }
---
+++ scripts/deploy_contract.sh
@@ -0,0 +1,8 @@
+casper-client put-deploy \
+--node-address http:/127.0.0.1:40101 \
+--secret-key ~/CasperLabs/casper-node/utils/nctl/assets/net-1/faucet/secret_key.pem \
+--chain-name casper-net-1 \
+--session-path ~/CasperLabs/Broadleaf-Demo/contract/broadleaf-messenger/contract/target/wasm32-unknown-unknown/release/contract.wasm \
+--session-arg "sender:string='George'"
+--session-arg "message:string='Hey hows it going for you?" \
+--payment-amount 1000000000 | jq
